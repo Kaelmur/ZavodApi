57b550d9ab35052f43c70b9ecbe714aa2a5cb792
Kael <a.murataliev@list.ru>
2024-05-28 21:21:16 +0500

    jwt token

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import getRoutes, getFractionPrices
 
 
 urlpatterns = [
-    path('', Home.as_view()),
+    path('', getRoutes),
+    path('prices/', getFractionPrices),
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,30 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.http import JsonResponse
+from .serializers import PriceSerializer
+from .models import FractionPrice
 
 
-class Home(APIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+@api_view(['GET'])
+def getRoutes(request):
 
-    def get(self, request):
-        content = {'message': 'Hello, World!'}
-        return Response(content)
+    routes = [
+        {'GET': '/api/fraction_prices'},
+        {'POST': '/api/fraction_prices'},
+
+        {'POST': '/api/users/token'},
+        {'POST': '/api/users/token/refresh'}
+    ]
+
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getFractionPrices(request):
+    fraction_prices = FractionPrice.objects.all()
+    serializer = PriceSerializer(fraction_prices, many=True)
+    return Response(serializer.data)
